3da2f41982a166f7fe1a44272b5711c61b1a2a22
vasiliy <vasiliy@fb.com>
2020-05-26 16:37:34 -0700

    fake_quantize: respect device affinity
    Summary:
    
    Makes the eager mode QAT prepare logic respect device affinity.
    This fixes the issue where a module is on `cuda:0`, and running
    the QAT prepare script would add observers on `cpu`.  Now it
    will add them on the original device.
    
    Test Plan:
    
    ```
    python test/test_quantization.py TestDistributed.test_device_affinity
    ```
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: b8754021ed7c5bb36e89732559f8601f4c4c9114
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/39031

--- test/quantization/test_workflow_module.py
+++ test/quantization/test_workflow_module.py
@@ -41,6 +41,7 @@ from torch.testing._internal.common_quantization import (
 from torch.testing._internal.common_quantized import (
     override_quantized_engine,
     supported_qengines,
+    override_qengines,
 )
 
 # Reference method for fake quantize
@@ -782,3 +783,37 @@ class TestDistributed(QuantizationTestCase):
             self.assertTrue(
                 isinstance(fused_model.conv.bn, nn.SyncBatchNorm),
                 "Expected BN to be converted to SyncBN")
+
+    @unittest.skipIf(not TEST_MULTIGPU, "multi-GPU not supported")
+    @unittest.skipIf(not TEST_CUDA, "CUDA unavailable")
+    @override_qengines
+    def test_device_affinity(self):
+        """
+        Tests that converting a model to QAT respects device affinity
+        """
+        class Model(nn.Module):
+
+            def __init__(self):
+                super(Model, self).__init__()
+                self.linear = nn.Linear(2, 2)
+
+            def forward(self, x):
+                x = self.linear(x)
+                return x
+
+        model = Model()
+        model.qconfig = torch.quantization.get_default_qat_qconfig(torch.backends.quantized.engine)
+        device = torch.device('cuda:0')
+        model.to(device)
+        torch.quantization.prepare_qat(model, inplace=True)
+        model_devices = {p.device for p in model.parameters()} | \
+            {p.device for p in model.buffers()}
+        self.assertEqual(len(model_devices), 1,
+                         message="Model must only have one device after QAT")
+        model_device = next(iter(model_devices))
+        self.assertEqual(model_device, device,
+                         message="QAT must respect device affinity")
+
+        # ensure that running an input on CUDA works without any needed changes
+        input = torch.randn(2, device=device)
+        model(input)
